40650e25341771ff3a84be0c2bf88a69dd7f9443
zoeypoole <89992581+zoeypoole@users.noreply.github.com>
2022-03-25 01:06:48 -0400

    javascript push

--- exercises/javascript/style.js
+++ exercises/javascript/style.js
@@ -7,34 +7,94 @@ darkModeButton.addEventListener("click", changeToDarkMode);
 function changeToDarkMode() {
   if(isDark === true) {
   
-  darkModeButton.textContent = "dark mode";
-  anythingIWant.style.background = "white";
-  anythingIWant.style.color = "black";
+  darkModeButton.textContent = "activate dark mode";
+  document.body.style.background = "#ff00a6";
+  document.body.style.color = "black";
   isDark = false;
   console.log("dark mode is off.");
     
   } else if(isDark === false){
     
-  darkModeButton.textContent = "light mode";
-  anythingIWant.style.background = "black";
-  anythingIWant.style.color = "white";
+  darkModeButton.textContent = "activate light mode";
+  document.body.style.background = "black";
+  document.body.style.color = "#ff00a6";
   isDark = true;
   console.log("dark mode is on!");
     
   }
 }
 
-const btn = document.getElementById('colorchange');
-
-btn.addEventListener('click', function onClick(event) {
-  document.body.style.color = 'green';
-});
-
-function myFunction() {
-    var x = document.getElementById("hello");
-    if (x.innerHTML === "Hello fellow New Yorker...") {
-      x.innerHTML = "don't you miss trees to?";
-    } else {
-      x.innerHTML = "Hello fellow New Yorker...";
-    }
+  let colorImage = document.getElementById("colorImage");
+  let button2 = document.getElementById("button2");
+  
+  function changeToBW() {
+  
+      if (colorImage.getAttribute('src') === "bimbo.png") {
+          colorImage.setAttribute('src', "tweet.png");
+      }
+      else {
+          colorImage.setAttribute('src', "bimbo.png");
+      }
   }
+  
+  button2.addEventListener("click", changeToBW);
+
+
+function toggleImage(){
+  document.querySelector('#image').classList.toggle('hidden');
+}
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function runClock(clock, timediff=0) {
+  
+
+  var now = new Date()
+  
+  
+  var hour = ((now.getUTCHours() + timediff) % 12)
+  
+ 
+  var min = now.getUTCMinutes()
+  var sec = now.getUTCSeconds()
+  var ms = now.getUTCMilliseconds()
+  
+  
+  var hourHand = clock.querySelector(".hour")
+  var minHand = clock.querySelector(".minute")
+  var secHand = clock.querySelector(".second")
+  
+  
+  var secRotation = (sec * 6) + (ms * 0.006)
+  
+ 
+  var minRotation = (min * 6) + (sec * 0.1)
+  
+ 
+  var hourRotation = (hour * 30) + (min * 0.1)
+  
+ 
+  hourHand.style.transform = "rotate(" + hourRotation + "deg)"
+  minHand.style.transform = "rotate(" + minRotation + "deg)"
+  secHand.style.transform = "rotate(" + secRotation + "deg)"
+  
+ 
+  requestAnimationFrame(function () {
+    runClock(clock, timediff)
+  })
+}
+runClock(document.querySelector(".clock-newyork"), -4)
+
