a4dd72dba927eed2307954cf156b67ebab1177a1
zoeypoole <89992581+zoeypoole@users.noreply.github.com>
2022-04-07 11:30:47 -0400

    js exercise

--- exercises/js/sketch.js
+++ exercises/js/sketch.js
@@ -22,47 +22,77 @@
 //create a variable to store the slider in
 
 
-let bgcolor;
-//create variable for input
-let input;
+// let bgcolor;
+// //create variable for input
+// let input;
+
+// function setup() {
+//   let canvas = createCanvas(windowWidth, windowHeight);
+//   frameRate(15);
+//   canvas.position(0, 0);
+//   canvas.class("container");
+
+//   //create a random fill
+//   bgcolor = {r: 0, g: 255, b:255};
+//   bgcolor.r = random(0, 255);
+//   bgcolor.g = random(0, 255);
+//   bgcolor.b = random(0,255);
+
+//   //create a div for our value
+//   // birthday = createDiv("Your birthday");
+//   // birthday.class("birthday");
+
+//   //create input
+//   input = createInput("whats your name");
+//   //adds a class to the input
+//   input.class("input");
+
+// }
+
+
+// function draw() {
+//   background(bgcolor.r,bgcolor.g,bgcolor.b);
+//   //target the HTML value of our variable birthday, and set the contents to be the value of our variable input
+//   // birthday.html(input.value());
+
+//   for (let i = 0; i < 320; i++) {
+//       push();
+//       fill(random(255), bgcolor.g, bgcolor.b);
+//       translate(random(width), random(height));
+//       rotate(random(5 * HALF_PI));
+//       textSize(20);
+//       text(input.value(), 10, 0);
+//       pop();
+//     }
+
+// }
 
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
-  frameRate(15);
-  canvas.position(0, 0);
-  canvas.class("container");
-
-  //create a random fill
-  bgcolor = {r: 0, g: 255, b:255};
-  bgcolor.r = random(0, 255);
-  bgcolor.g = random(0, 255);
-  bgcolor.b = random(0,255);
-
-  //create a div for our value
-  // birthday = createDiv("Your birthday");
-  // birthday.class("birthday");
-
-  //create input
-  input = createInput("whats your name");
-  //adds a class to the input
-  input.class("input");
-
+  createCanvas(windowWidth, windowHeight);
 }
 
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+let circleBgcolor = {r: 500, g: 0, b:0};
+
+
+// function circleColor() {
+//   circleBgcolor.r = red(225, 255);
+//   circleBgcolor.g = red(225, 255);
+//   circleBgcolor.b = red(225, 255);
+// }
+
+
 function draw() {
-  background(bgcolor.r,bgcolor.g,bgcolor.b);
-  //target the HTML value of our variable birthday, and set the contents to be the value of our variable input
-  // birthday.html(input.value());
+  noStroke();
+  fill(circleBgcolor.r,circleBgcolor.g,circleBgcolor.b);
+  ellipse(mouseX, mouseY, 5, 5);
+}
 
-  for (let i = 0; i < 320; i++) {
-      push();
-      fill(random(255), bgcolor.g, bgcolor.b);
-      translate(random(width), random(height));
-      rotate(random(5 * HALF_PI));
-      textSize(20);
-      text(input.value(), 10, 0);
-      pop();
-    }
-
+function mousePressed() {
+  clear();
+  circleColor(red);
 }
